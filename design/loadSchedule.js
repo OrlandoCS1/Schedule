0086a5c016dc35eadeb65f99ab9f7083f75d8dcc
OrlandoCS <orlandcalderons@gmail.com>
2026-08-16 23:00:14 -0600

    Bubble Task succesfully implemented alonside add form, * Bug fixed: grid-row is now already setting hours correctly

--- design/loadSchedule.js
+++ design/loadSchedule.js
@@ -1,12 +1,13 @@
 const schedule = [];
 
 class Task {
-    constructor(title, notes, category, dayWeek, startHour, startHourValue, endHourValue, color){
+    constructor(title, notes, category, dayWeek, startHour, endHour, startHourValue, endHourValue, color){
         this.title = title;
         this.notes = notes;
         this.category = category;
         this.dayWeek = dayWeek;
         this.startHour = startHour;
+        this.endHour = endHour;
         this.startHourValue = startHourValue;
         this.endHourValue = endHourValue;
         this.color = color;
